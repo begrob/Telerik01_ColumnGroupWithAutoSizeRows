4594a03acccd05f44ec13fbe4b2ca44e8217c881
Robert Began <robert.began@epicor.com>
2020-10-14 09:33:51 -0700

    Original code from the ticket post of 2020-10-14

---
+++ 1486866GridViewDefs/RadForm1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace _1486866GridViewDefs
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.nwindDataSet = new _1486866GridViewDefs.NwindDataSet();
+            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customersTableAdapter = new _1486866GridViewDefs.NwindDataSetTableAdapters.CustomersTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            this.radGridView1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.Controls.Add(this.radButton2);
+            this.radGridView1.Controls.Add(this.radButton1);
+            this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radGridView1.Location = new System.Drawing.Point(0, 0);
+            // 
+            // 
+            // 
+            this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition1;
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.Size = new System.Drawing.Size(1100, 490);
+            this.radGridView1.TabIndex = 0;
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(4, 4);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(110, 24);
+            this.radButton1.TabIndex = 1;
+            this.radButton1.Text = "radButton1";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(121, 4);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(110, 24);
+            this.radButton2.TabIndex = 2;
+            this.radButton2.Text = "radButton2";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // nwindDataSet
+            // 
+            this.nwindDataSet.DataSetName = "NwindDataSet";
+            this.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersBindingSource
+            // 
+            this.customersBindingSource.DataMember = "Customers";
+            this.customersBindingSource.DataSource = this.nwindDataSet;
+            // 
+            // customersTableAdapter
+            // 
+            this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 490);
+            this.Controls.Add(this.radGridView1);
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "RadForm1";
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            this.radGridView1.ResumeLayout(false);
+            this.radGridView1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private NwindDataSet nwindDataSet;
+        private System.Windows.Forms.BindingSource customersBindingSource;
+        private NwindDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+    }
+}
---
+++ 1486866GridViewDefs/RadForm1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace _1486866GridViewDefs
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'nwindDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.nwindDataSet.Customers);
+
+            this.radGridView1.DataSource = this.customersBindingSource;
+            this.radGridView1.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.Fill;
+
+            foreach (GridViewColumn col in this.radGridView1.Columns)
+            {
+                col.WrapText = true;
+            }
+             
+            ColumnGroupsViewDefinition view = new ColumnGroupsViewDefinition();
+            view.ColumnGroups.Add(new GridViewColumnGroup("Customer Contact"));
+            view.ColumnGroups.Add(new GridViewColumnGroup("Details"));
+            view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Address"));
+            view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Contact"));
+
+            view.ColumnGroups[0].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("CompanyName");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactName");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactTitle");
+            view.ColumnGroups[1].Groups[0].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Address");
+            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("City");
+            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Country");
+            view.ColumnGroups[1].Groups[1].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Phone");
+            view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Fax");
+            radGridView1.ViewDefinition = view;
+
+            //this.radGridView1.Columns["CustomerID"].HeaderText = "";
+            //this.radGridView1.Columns["CustomerID"].HeaderImage = Properties.Resources.Alarm2;
+
+            this.radGridView1.AutoSizeRows = true;
+
+
+
+
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            HtmlViewDefinition view = new HtmlViewDefinition();
+            view.RowTemplate.Rows.Add(new RowDefinition());
+            view.RowTemplate.Rows.Add(new RowDefinition());
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("CustomerID"));
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("CompanyName"));
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("City"));
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("Country"));
+            view.RowTemplate.Rows[1].Cells.Add(new CellDefinition("Phone"));
+            view.RowTemplate.Rows[1].Cells[0].ColSpan = 2;
+            view.RowTemplate.Rows[0].Cells[2].RowSpan = 2;
+            view.RowTemplate.Rows[0].Cells[3].RowSpan = 2;
+            view.RowTemplate.Rows[0].Height = 40;
+            radGridView1.ViewDefinition = view;
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            HtmlViewDefinition view = new HtmlViewDefinition();
+            view.RowTemplate.Rows.Add(new RowDefinition());
+            view.RowTemplate.Rows.Add(new RowDefinition());            
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("City"));
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("Country"));    
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("CompanyName"));
+            view.RowTemplate.Rows[0].Cells.Add(new CellDefinition("CustomerID"));
+
+
+            view.RowTemplate.Rows[1].Cells.Add(new CellDefinition("Phone"));
+            view.RowTemplate.Rows[1].Cells[0].ColSpan = 2;
+            view.RowTemplate.Rows[0].Cells[2].RowSpan = 2;
+            view.RowTemplate.Rows[0].Cells[3].RowSpan = 2;
+            view.RowTemplate.Rows[0].Height = 40;
+            radGridView1.ViewDefinition = view;
+        }
+    }
+}
