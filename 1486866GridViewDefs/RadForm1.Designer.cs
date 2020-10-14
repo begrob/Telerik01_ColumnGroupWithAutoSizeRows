7ba066af1d938a2ffc9702f476d4a6bd95c41f12
Robert Began <robert.began@epicor.com>
2020-10-14 10:19:19 -0700

    Grid now in TableView by default. Two buttons added to switch to 2 different ColumnGroupsViews. Also a button to switch back to TableView.
    This code still works as expected.

--- 1486866GridViewDefs/RadForm1.Designer.cs
+++ 1486866GridViewDefs/RadForm1.Designer.cs
@@ -31,23 +31,32 @@
             this.components = new System.ComponentModel.Container();
             Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
             this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
-            this.radButton1 = new Telerik.WinControls.UI.RadButton();
             this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
             this.nwindDataSet = new _1486866GridViewDefs.NwindDataSet();
             this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.customersTableAdapter = new _1486866GridViewDefs.NwindDataSetTableAdapters.CustomersTableAdapter();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.radButton5 = new Telerik.WinControls.UI.RadButton();
             ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
             this.radGridView1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
             // radGridView1
             // 
+            this.radGridView1.Controls.Add(this.radButton5);
+            this.radGridView1.Controls.Add(this.radButton4);
+            this.radGridView1.Controls.Add(this.radButton3);
             this.radGridView1.Controls.Add(this.radButton2);
             this.radGridView1.Controls.Add(this.radButton1);
             this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -55,19 +64,11 @@
             // 
             // 
             // 
+            this.radGridView1.MasterTemplate.EnableAlternatingRowColor = true;
             this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition1;
             this.radGridView1.Name = "radGridView1";
             this.radGridView1.Size = new System.Drawing.Size(1100, 490);
             this.radGridView1.TabIndex = 0;
-            // 
-            // radButton1
-            // 
-            this.radButton1.Location = new System.Drawing.Point(4, 4);
-            this.radButton1.Name = "radButton1";
-            this.radButton1.Size = new System.Drawing.Size(110, 24);
-            this.radButton1.TabIndex = 1;
-            this.radButton1.Text = "radButton1";
-            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
             // 
             // radButton2
             // 
@@ -75,8 +76,17 @@
             this.radButton2.Name = "radButton2";
             this.radButton2.Size = new System.Drawing.Size(110, 24);
             this.radButton2.TabIndex = 2;
-            this.radButton2.Text = "radButton2";
+            this.radButton2.Text = "HtmlView 2";
             this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(4, 4);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(110, 24);
+            this.radButton1.TabIndex = 1;
+            this.radButton1.Text = "HtmlView 1";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
             // 
             // nwindDataSet
             // 
@@ -91,6 +101,33 @@
             // customersTableAdapter
             // 
             this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(237, 4);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(110, 24);
+            this.radButton3.TabIndex = 3;
+            this.radButton3.Text = "ColGroupView 1";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // radButton4
+            // 
+            this.radButton4.Location = new System.Drawing.Point(353, 4);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(110, 24);
+            this.radButton4.TabIndex = 4;
+            this.radButton4.Text = "ColGroupView 2";
+            this.radButton4.Click += new System.EventHandler(this.radButton4_Click);
+            // 
+            // radButton5
+            // 
+            this.radButton5.Location = new System.Drawing.Point(469, 4);
+            this.radButton5.Name = "radButton5";
+            this.radButton5.Size = new System.Drawing.Size(110, 24);
+            this.radButton5.TabIndex = 5;
+            this.radButton5.Text = "TableView (orig)";
+            this.radButton5.Click += new System.EventHandler(this.radButton5_Click);
             // 
             // RadForm1
             // 
@@ -109,10 +146,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
             this.radGridView1.ResumeLayout(false);
             this.radGridView1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
@@ -126,5 +166,8 @@
         private NwindDataSet nwindDataSet;
         private System.Windows.Forms.BindingSource customersBindingSource;
         private NwindDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private Telerik.WinControls.UI.RadButton radButton5;
     }
 }
--- 1486866GridViewDefs/RadForm1.cs
+++ 1486866GridViewDefs/RadForm1.cs
@@ -13,10 +13,13 @@ namespace _1486866GridViewDefs
 {
     public partial class RadForm1 : Telerik.WinControls.UI.RadForm
     {
+        private IGridViewDefinition _originalView;
+
         public RadForm1()
         {
             InitializeComponent();
         }
+
 
         private void RadForm1_Load(object sender, EventArgs e)
         {
@@ -31,33 +34,30 @@ namespace _1486866GridViewDefs
                 col.WrapText = true;
             }
              
-            ColumnGroupsViewDefinition view = new ColumnGroupsViewDefinition();
-            view.ColumnGroups.Add(new GridViewColumnGroup("Customer Contact"));
-            view.ColumnGroups.Add(new GridViewColumnGroup("Details"));
-            view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Address"));
-            view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Contact"));
+            //ColumnGroupsViewDefinition view = new ColumnGroupsViewDefinition();
+            //view.ColumnGroups.Add(new GridViewColumnGroup("Customer Contact"));
+            //view.ColumnGroups.Add(new GridViewColumnGroup("Details"));
+            //view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Address"));
+            //view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Contact"));
 
-            view.ColumnGroups[0].Rows.Add(new GridViewColumnGroupRow());
-            view.ColumnGroups[0].Rows[0].ColumnNames.Add("CompanyName");
-            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactName");
-            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactTitle");
-            view.ColumnGroups[1].Groups[0].Rows.Add(new GridViewColumnGroupRow());
-            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Address");
-            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("City");
-            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Country");
-            view.ColumnGroups[1].Groups[1].Rows.Add(new GridViewColumnGroupRow());
-            view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Phone");
-            view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Fax");
-            radGridView1.ViewDefinition = view;
+            //view.ColumnGroups[0].Rows.Add(new GridViewColumnGroupRow());
+            //view.ColumnGroups[0].Rows[0].ColumnNames.Add("CompanyName");
+            //view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactName");
+            //view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactTitle");
+            //view.ColumnGroups[1].Groups[0].Rows.Add(new GridViewColumnGroupRow());
+            //view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Address");
+            //view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("City");
+            //view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Country");
+            //view.ColumnGroups[1].Groups[1].Rows.Add(new GridViewColumnGroupRow());
+            //view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Phone");
+            //view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Fax");
+            //radGridView1.ViewDefinition = view;
 
             //this.radGridView1.Columns["CustomerID"].HeaderText = "";
             //this.radGridView1.Columns["CustomerID"].HeaderImage = Properties.Resources.Alarm2;
 
             this.radGridView1.AutoSizeRows = true;
-
-
-
-
+            _originalView = this.radGridView1.ViewDefinition;
         }
 
         private void radButton1_Click(object sender, EventArgs e)
@@ -94,6 +94,50 @@ namespace _1486866GridViewDefs
             view.RowTemplate.Rows[0].Cells[3].RowSpan = 2;
             view.RowTemplate.Rows[0].Height = 40;
             radGridView1.ViewDefinition = view;
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            ColumnGroupsViewDefinition view = new ColumnGroupsViewDefinition();
+            view.ColumnGroups.Add(new GridViewColumnGroup("Customer Contact"));
+            view.ColumnGroups.Add(new GridViewColumnGroup("Details"));
+            view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Address"));
+            view.ColumnGroups[1].Groups.Add(new GridViewColumnGroup("Contact"));
+
+            view.ColumnGroups[0].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("CompanyName");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactName");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactTitle");
+            view.ColumnGroups[1].Groups[0].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Address");
+            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("City");
+            view.ColumnGroups[1].Groups[0].Rows[0].ColumnNames.Add("Country");
+            view.ColumnGroups[1].Groups[1].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Phone");
+            view.ColumnGroups[1].Groups[1].Rows[0].ColumnNames.Add("Fax");
+            radGridView1.ViewDefinition = view;
+        }
+
+        private void radButton4_Click(object sender, EventArgs e)
+        {
+            ColumnGroupsViewDefinition view = new ColumnGroupsViewDefinition();
+            view.ColumnGroups.Add(new GridViewColumnGroup("Group Header"));
+            view.ColumnGroups[0].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactName");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("ContactTitle");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("Address");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("City");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("Country");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("Phone");
+            view.ColumnGroups[0].Rows[0].ColumnNames.Add("Fax");
+            view.ColumnGroups[0].Rows.Add(new GridViewColumnGroupRow());
+            view.ColumnGroups[0].Rows[1].ColumnNames.Add("CompanyName");
+            radGridView1.ViewDefinition = view;
+        }
+
+        private void radButton5_Click(object sender, EventArgs e)
+        {
+            radGridView1.ViewDefinition = _originalView;
         }
     }
 }
